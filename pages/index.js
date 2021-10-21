e695ec1186993e366a414bde57176ada1b13d037
Tom Gallagher <tom.gallagher@theappengineers.com>
2021-10-21 09:31:25 +0100

    add tailwind

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,16 @@
 import Head from 'next/head'
-import Header from '@components/Header'
 import Footer from '@components/Footer'
 
 export default function Home() {
   return (
-    <div className="container">
+    <div className="container antialiased">
       <Head>
         <title>Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi scelerisque dui at dapibus tincidunt. In eleifend ante sit amet rhoncus cursus. Nullam erat lacus, ultrices vel molestie eu, luctus id enim.
       </main>
 
       <Footer />
